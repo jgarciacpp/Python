753ce709cf67e8cff227d957850362ad909f2c81
jgarciacpp <jgarci85@gmail.com>
2026-04-13 16:34:44 -0400

    Finalized Test Code

--- Scripts/currentSensor.py
+++ Scripts/currentSensor.py
@@ -1,23 +1,52 @@
-from gpiozero import MCP3008
-from time import sleep
+import spidev
+import time
+from collections import deque
 
-# This connects to Channel 0 of the MCP3008
-# It assumes you are using CE0 (GPIO 8) for Chip Select
-sensor = MCP3008(channel=0)
 
-print("Reading current sensor... Press Ctrl+C to stop.")
+
+# --- SPI Setup ---
+spi = spidev.SpiDev()
+spi.open(0, 0) # Bus 0, CE0
+spi.max_speed_hz = 1350000 # Safest Answer for 3.3V System (1.35MHz)
+spi.mode = 0 # Mode 0 is being used, Required
+
+
+# --- Read one channel ---
+def read_adc(channel):
+    adc = spi.xfer2([1, (8 + channel) << 4, 0]) # [Start Bit, Configuration Bit, Dont Care]
+    value = ((adc[1] & 3) << 8) + adc[2] # [Have first 2 Bits masked by 3, and shifted 8 to left. Then 8 Bits are added at the end]
+    return value # [The final raw digital value]
+
+
+# --- Target sample rate ---
+sample_rate = 2000 # 2 kHz
+sample_period = 1.0 / sample_rate
+
+print("Sampling at ~2 kHz. Press Ctrl+C to stop.")
+
+next_time = time.perf_counter()
+
+voltage_buffer = deque(maxlen=100) # [Buffer that holds 100 items]
 
 try:
+    counter = 0 # [Counter for while loop]
     while True:
-        # .value returns a float between 0.0 and 1.0
-        # representing the percentage of VREF
-        raw_value = sensor.value
-        
-        # To get the actual voltage:
-        voltage = raw_value * 3.3
-        
-        print(f"Raw: {raw_value:.4f} | Voltage: {voltage:.2f}V")
-        sleep(0.5)
+        counter += 1
+        raw_value = read_adc(0)
+        voltage = (raw_value / 1023.0) * 3.3
+        voltage_buffer.append(voltage)
+        avg_voltage = sum(voltage_buffer) / len(voltage_buffer)
+    
+        # Maintain stable timing
+        next_time += sample_period
+        sleep_time = next_time - time.perf_counter()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+    
+        if counter % 20 == 0:
+            print(f"Raw: {avg_voltage:.4f}V")
+
 
 except KeyboardInterrupt:
-    print("\nProgram stopped.")
+    spi.close()
+    print("Program Stopped.")
